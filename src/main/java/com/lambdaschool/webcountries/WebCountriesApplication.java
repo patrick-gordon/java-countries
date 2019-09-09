c90f335e9b018d850b9c778c1fd18935645b4f5e
Patrick Gordon <patrickgordon42@yahoo.com>
2019-09-09 17:04:29 -0500

    initial commit

---
+++ src/main/java/com/lambdaschool/webcountries/WebCountriesApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.webcountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCountriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCountriesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lambdaschool/webcountries/WebCountriesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lambdaschool.webcountries;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebCountriesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
